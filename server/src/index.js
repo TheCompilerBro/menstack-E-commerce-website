30b252c485d5c599a4806b62b2403bc23a94e275
TheCompilerBro <puspendu0004@icloud.com>
2026-04-07 23:16:25 +0530

    fix conflict

--- server/src/index.js
+++ server/src/index.js
@@ -27,7 +27,7 @@ app.use((err, _, res, __) => {
   res.status(500).json({ message: 'Server error' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
